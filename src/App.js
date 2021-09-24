0b98da216cb9b6148ae4200a7a54959e270c4c86
ChMenguy <58273786+ChMenguy@users.noreply.github.com>
2021-09-24 09:59:31 +0200

    up

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import Banner from './Component/Banner';
-import About from './Component/About';
-import Service from './Component/Service';
-import PricingPlan from './Component/PricingPlan';
-import Contact from './Component/Contact';
-import Footer from './Component/Footer';
-import Portfolio from './Component/Portfolio';
-import Navbar from './Component/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import './Assets/Css/Responsive.css';
+import Display from '../src/Display'
 
 function App() {
     return (
-        <div className="App">
-            <Navbar />
-            <Banner/>
-            <About/>
-            <Service/>
-            <Portfolio/>
-            <PricingPlan/>
-            <Contact/>
-            <Footer/>
-        </div>
+        <Display />
     );
 }
 
